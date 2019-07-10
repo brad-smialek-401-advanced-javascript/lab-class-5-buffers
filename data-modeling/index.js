23a5213880b4b5695f8d7bccac895b03a31955db
bjwine0 <bradsmilek@gmail.com>
2019-07-09 23:41:43 -0700

    built out code from demo code and testing

--- data-modeling/index.js
+++ data-modeling/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const Categories = require('./models-singular/categories.js')
 
 // Require your model
 
@@ -8,9 +9,36 @@ const mongoose = require('mongoose');
 const MONGOOSE_URI = 'mongodb://localhost:27017/class05';
 
 // Connect
-mongoose.connect(...);
+mongoose.connect(MONGOOSE_URI, { useNewUrlParser: true });
+
+let categories = new Categories();
+
 
 // Do some work
 
-// Disconnect
-mongoose.disconnect();
+const doDataStuff = async () => {
+
+  let sampleCategory = {
+    name: 'furniture',
+    description: 'chair',
+    
+  };
+
+  let newCategory = await categories.create(sampleCategory);
+  console.log('Category Created', newCategory);
+
+
+  let allCategories = await categories.get()
+  console.log('All Categories', allCategories);
+
+  let oneCategory = await categories.get('5d12a3c0e27a289b9bd5eca4');
+  console.log('One Category', oneCategory);
+
+  // Disconnect 
+  mongoose.disconnect();
+
+};
+
+doDataStuff();
+
+
--- data-modeling/models-singular/categories-schema.js
+++ data-modeling/models-singular/categories-schema.js
@@ -4,7 +4,22 @@ const mongoose = require('mongoose');
 
 // What fields and constraints do we want?
 const categories = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true, uppercase: true, enum: ['P', 'C', '1B', '2B', '3B', 'SS', 'LF', 'RF', 'CF'] },
 
+});
+
+categories.post('findOne', function (doc) {
+  doc.name = doc.name.toUpperCase();
+});
+
+categories.post('init', function () {
+  // this.name = this.name.toUpperCase(); // <- this will break our tests (that's a good thing)
+  console.log(this);
+});
+
+categories.post('save', function () {
+  console.log('saving', this);
 });
 
 // Do we need to run any lifecycle hooks/middleware?
